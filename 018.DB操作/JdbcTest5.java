70471db186671cbc10e44ce5a4cc72c040f2b788
kekenji <k.i.11.22.1994@gmail.com>
2018-03-08 17:21:45 +0900

    課題提出

---
+++ 018.DB操作/JdbcTest5.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+import java.sql.*;
+
+/**
+ *
+ * @author geust1Day
+ */
+
+public class JdbcTest5 {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost/Challenge_db";
+            String user = "Challenge";
+            String password = "19941122";
+
+            System.out.println("URL      :" + url);
+            System.out.println("USER     :" + user);
+            System.out.println("PASSWORD :" + password);
+
+            System.out.println("データベースに接続します...");
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            System.out.println("データベースと接続しました");
+
+            //データベース表示
+            Statement stm = conn.createStatement();
+            String sql = "select * from profiles where profilesID = 1";
+            ResultSet rs = stm.executeQuery(sql);
+            
+            
+            while (rs.next()) {
+                int id = rs.getInt("profilesID");
+                String name = rs.getString("name");
+                String tel = rs.getString("tel");
+                String age = rs.getString("age");
+                String birthday = rs.getString("birthday");
+                System.out.println("取得結果 -> " + id + ":" + name+":"+tel+":"+age+":"+birthday);
+               
+            }
+            
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
